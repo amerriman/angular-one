2e5a5bf7f4a83129b08dd76db916dcb10145add4
amerriman <amm434@gmail.com>
2015-09-09 16:21:34 -0600

    finished first three sections

---
+++ js/vanilla.js
@@ -0,0 +1,19 @@
+$(document).on('ready', function() {
+
+// //HTML
+//   <form>
+//     <input type="text" id="vname" placeholder="What is your name?">
+//     <button type="submit" class="btn btn-danger">Submit</button>
+//     </form>
+//     <br>
+//     <h1>My name is:<span id="uname"></span></h1>
+// //END HTML
+
+  // $('form').on('submit', function(){
+  //   var $name = $("#vname").val();
+  //   console.log($name);
+  //   $("#uname").text($name);
+  // });
+
+});
+
